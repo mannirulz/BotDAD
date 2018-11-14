628dbb1e36e28345d489cc74a3b1cf6ebfcf2596
mannirulz <mannirulz@gmail.com>
2018-11-14 12:41:46 +0530

    Update Threshold.py

--- Threshold.py
+++ Threshold.py
@@ -1,3 +1,24 @@
+# Copyright (C) 2016   Manmeet Singh, Maninder Singh, Sanmeet kour
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose with or without fee is hereby granted,
+# provided that the above copyright notice and this permission notice
+# appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+# !python2
+
+# Experiment to check DNS query of top 100 Alexa Domains
+
+
 import webbrowser
 import os
 import time
@@ -116,7 +137,10 @@ for key, value in whitelist.iteritems():
     clear_cache = subprocess.check_output("ipconfig /flushdns", shell=True)
     #output = subprocess.check_output("c:/Progra~1/Wireshark/tshark -q -i 1  -Y ""dns.flags.response==0"" -a duration:5", shell=False)
     #webbrowser.get('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s').open(key)
-    webbrowser.open("kat.cr", new=1, autoraise=False)
+    #webbrowser.open("facebook,com", new=1, autoraise=False)
+    
+    webbrowser.open(key, new=1, autoraise=False)
+    
     time.sleep(10)
     output = subprocess.check_output("ipconfig /displaydns", shell=True)
     #print(output)
