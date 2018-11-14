0599021f989e705745b42a035f1bd25fca709cb1
mannirulz <mannirulz@gmail.com>
2018-11-14 12:16:25 +0530

    Update BotSummary.py

--- BotSummary.py
+++ BotSummary.py
@@ -13,7 +13,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
 # OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-# Main
+# To print summary of bot and clean count from DNS_FP_Anomaly
 #
 
 # !python2
